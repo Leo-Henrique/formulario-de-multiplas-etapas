2e0c25d7000cbb5b15d451ce7768a5c5f2e5729b
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-03-01 13:14:08 -0300

    Created all components. Finished static HTML with JSX

---
+++ src/components/Finished.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SVGThankYou from "../assets/icon-thank-you.svg";
+
+export default function Finished() {
+    return (
+        <section>
+            <div>
+                <SVGThankYou />
+            </div>
+
+            <h1>Obrigado!</h1>
+
+            <p>
+                Obrigado por confirmar sua inscrição! Esperamos que você se divirta usando nossa plataforma!
+            </p>
+        </section>
+    );
+}
